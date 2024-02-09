1283923ea8733205b1c7dc451a93e87f82c513f5
Nithin-Rajkumar <113581043+Nithin-Rajkumar@users.noreply.github.com>
2024-02-09 14:41:00 +0530

    Delete src directory

--- src/main/java/com/bec/nithin/repository/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bec.nithin.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bec.nithin.model.Course;
-
-public interface CourseRepository extends JpaRepository <Course,String>{
-
-}
--- src/main/java/com/bec/nithin/repository/PaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bec.nithin.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bec.nithin.model.Payment;
-
-public interface PaymentRepository extends JpaRepository <Payment,String>{
-
-}
